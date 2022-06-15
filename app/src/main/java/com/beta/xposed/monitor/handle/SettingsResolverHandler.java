a513c0cf989ce7f909b3550888939770daa66b45
wangbaosheng <wbaosheng.wang@qq.com>
2022-06-15 17:26:27 +0800

    filter android_id

--- app/src/main/java/com/beta/xposed/monitor/handle/SettingsResolverHandler.java
+++ app/src/main/java/com/beta/xposed/monitor/handle/SettingsResolverHandler.java
@@ -10,7 +10,10 @@ public class SettingsResolverHandler extends MethodHandler {
 
     @Override
     protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
-        Log.i(PrivacyMonitor.TAG, PrivacyMonitor.processName + "**检测到风险Settings查询: " + param.args[1].toString());
-        super.beforeHookedMethod(param);
+        String str = param.args[1].toString();
+        Log.i(PrivacyMonitor.TAG + "-Settings", PrivacyMonitor.processName + "**检测到风险Settings查询: " + str);
+        if ("android_id".equalsIgnoreCase(str)) {
+            super.beforeHookedMethod(param);
+        }
     }
 }
